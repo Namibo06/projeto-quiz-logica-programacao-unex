320072a36ffa69852517b1ec2ea27e9c0d44fa29
Namibo06 <namibo654@gmail.com>
2024-09-17 15:35:26 -0300

    refactor: helpTips

--- src/js/helpTips.js
+++ src/js/helpTips.js
@@ -1,16 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const helpTips = document.getElementById('help-tips');
+    const question = document.getElementById('question');
     const openHelpTips = document.getElementById('open-help-tips');
-    const closeHelpTips = document.getElementById('close-help-tips');
+    const showTime = document.getElementById('show-time');
 
     openHelpTips.addEventListener('click', () => {
-        openHelpTips.style.display="none";
-        helpTips.style.display="block";
-    });
+        const helpTips = "Indica a união entre duas proposições, sendo verdadeiro quando pelo menos uma delas for verdadeira."
+        question.textContent=helpTips;
 
-    closeHelpTips.addEventListener('click',() => {
-        openHelpTips.style.display="block";
-        helpTips.style.display="none";
+       
+        for (let i = 10; i >= 0; i--) {
+            setTimeout(() => {
+                if(i >= 10){
+                    showTime.textContent="00:"+i;
+                }
+                else if(i === 0){
+                    showTime.textContent="";
+                }else{
+                    showTime.textContent="00:0"+i;
+                }   
+
+                
+            }, (10 - i) * 1000);
+        }
+
+        setTimeout(() => {
+            const titleQuestion = 'Qual é o nome do operador lógico representado pelo símbolo "V"?';
+            question.textContent=titleQuestion;
+        }, 10000);
     });
 });
 
