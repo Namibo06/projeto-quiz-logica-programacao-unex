615bb1d6325a245512d868f6c7caf9e065be2502
Namibo06 <namibo654@gmail.com>
2024-09-17 15:58:33 -0300

    feat: zero points if logout game

--- src/js/resultPage1.js
+++ src/js/resultPage1.js
@@ -4,6 +4,13 @@ document.addEventListener('DOMContentLoaded',()=>{
     const condicional = document.getElementById('condicional');
     const bicondicional = document.getElementById('bicondicional');
     let totalPoints = 0;
+    const outGame = document.getElementById('out-game');
+
+    outGame.addEventListener('click',()=>{
+        totalPoints = 0;
+
+        localStorage.setItem('totalPoints',totalPoints);
+    });
 
     disjuncao.addEventListener('click',()=>{
         totalPoints = 10;
@@ -18,6 +25,13 @@ document.addEventListener('DOMContentLoaded',()=>{
         condicional.style.color="#f5f5f5";
         bicondicional.style.backgroundColor="#ff0000";
         bicondicional.style.color="#f5f5f5";
+
+        disjuncao.style.border="2px solid #f5f5f5";
+
+        disjuncao.innerHTML="Disjunção  10pts";
+        conjuncao.innerHTML="Conjunção  0pts";
+        condicional.innerHTML="Condicional  0pts";
+        bicondicional.innerHTML="Bi-condicional 0pts";
 
         setTimeout(() => {
             window.location.href="page2.html";
@@ -37,6 +51,12 @@ document.addEventListener('DOMContentLoaded',()=>{
         condicional.style.color="#f5f5f5";
         bicondicional.style.backgroundColor="#ff0000";
         bicondicional.style.color="#f5f5f5";
+
+        conjuncao.style.border="2px solid #f5f5f5";
+        disjuncao.innerHTML="Disjunção  10pts";
+        conjuncao.innerHTML="Conjunção  0pts";
+        condicional.innerHTML="Condicional  0pts";
+        bicondicional.innerHTML="Bi-condicional 0pts";
 
         setTimeout(() => {
             window.location.href="page2.html";
@@ -58,6 +78,12 @@ document.addEventListener('DOMContentLoaded',()=>{
         bicondicional.style.backgroundColor="#ff0000";
         bicondicional.style.color="#f5f5f5";
 
+        condicional.style.border="2px solid #f5f5f5";
+        disjuncao.innerHTML="Disjunção  10pts";
+        conjuncao.innerHTML="Conjunção  0pts";
+        condicional.innerHTML="Condicional  0pts";
+        bicondicional.innerHTML="Bi-condicional 0pts";
+
         setTimeout(() => {
             window.location.href="page2.html";
         }, 3000);
@@ -76,6 +102,12 @@ document.addEventListener('DOMContentLoaded',()=>{
         condicional.style.color="#f5f5f5";
         bicondicional.style.backgroundColor="#ff0000";
         bicondicional.style.color="#f5f5f5";
+
+        bicondicional.style.border="2px solid #f5f5f5";
+        disjuncao.innerHTML="Disjunção  10pts";
+        conjuncao.innerHTML="Conjunção  0pts";
+        condicional.innerHTML="Condicional  0pts";
+        bicondicional.innerHTML="Bi-condicional 0pts";
 
         setTimeout(() => {
             window.location.href="page2.html";
